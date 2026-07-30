6a74a50de4c86bb38c7fbe4519e9b83c8f990332
Sriram A S <itsriramas@gmail.com>
2026-07-30 13:00:48 +0530

    begin the java track

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome to Java Track");
-       System.out.println("I already know to build programming logic");
-       System.out.println("Now I'm learning how to express that logic using Java");
+        System.out.println("Welcome to Java Track");
+        System.out.println("My name is Sriram");
+        System.out.println("I already know to build programming logic");
+        System.out.println("Now I'm learning how to express that logic using Java");
     }
 }
