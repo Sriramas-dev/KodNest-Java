7fe2300c98a485685223b34c14e5996657ec8688
Sriram A S <itsriramas@gmail.com>
2026-08-05 12:22:06 +0530

    switchcase

---
+++ track/M02/T02/ST03/L03/SwitchPgm.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class SwitchPgm {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = scan.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid number");
+        }
+    }
+
+}
